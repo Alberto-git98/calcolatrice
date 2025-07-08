216ba820868d824db88f4a6ab1281a03680c19b3
Alberto Di Coste <albertjazz21@gmail.com>
2025-07-08 15:50:36 +0200

    Sostituito contenuto con calcolatrice interattiva

--- calcolatrice.py
+++ calcolatrice.py
@@ -1,1 +1,56 @@
-print('Benvenuto nella calcolatrice!')
+
+import math
+
+def somma(a, b):
+    return a + b
+
+def sottrazione(a, b):
+    return a - b
+
+def moltiplicazione(a, b):
+    return a * b
+
+def divisione(a, b):
+    if b == 0:
+        return "Errore: divisione per zero"
+    return a / b
+
+def radice(a):
+    if a < 0:
+        return "Errore: numero negativo"
+    return math.sqrt(a)
+
+print("=== Calcolatrice ===")
+
+while True:
+    print("\nOperazioni disponibili:")
+    print("1. Somma")
+    print("2. Sottrazione")
+    print("3. Moltiplicazione")
+    print("4. Divisione")
+    print("5. Radice quadrata")
+    print("6. Esci")
+
+    scelta = input("Scegli un'opzione (1-6): ")
+
+    if scelta == "6":
+        print("Uscita dal programma.")
+        break
+
+    if scelta == "5":
+        numero = float(input("Numero: "))
+        print("Risultato:", radice(numero))
+    else:
+        a = float(input("Numero 1: "))
+        b = float(input("Numero 2: "))
+
+        if scelta == "1":
+            print("Risultato:", somma(a, b))
+        elif scelta == "2":
+            print("Risultato:", sottrazione(a, b))
+        elif scelta == "3":
+            print("Risultato:", moltiplicazione(a, b))
+        elif scelta == "4":
+            print("Risultato:", divisione(a, b))
+        else:
+            print("Scelta non valida.")
